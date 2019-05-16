56018efa0ecb6dbe9bd7f1815e7740d7325f1fd8
Matthew Burger <mburger@stsci.edu>
2019-05-16 09:42:24 -0400

    sets up database

--- MESSENGERuvvs/__init__.py
+++ MESSENGERuvvs/__init__.py
@@ -6,7 +6,7 @@ from .database_setup import messenger_database_setup
 name = 'MESSENGERuvvs'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 
 try:
