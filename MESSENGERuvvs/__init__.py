3c8d21be60cf832309237bd077cb153de4180fcd
Matthew Burger <mburger@stsci.edu>
2021-02-08 11:44:37 -0500

    Support for LOSResults.py updates

--- MESSENGERuvvs/__init__.py
+++ MESSENGERuvvs/__init__.py
@@ -7,7 +7,7 @@ from .initialize_MESSENGERdata import initialize_MESSENGERdata
 name = 'MESSENGERuvvs'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '1.4.15'
+__version__ = '1.5.0'
 
 
 messenger_database_setup()
