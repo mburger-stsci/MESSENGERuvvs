f4b41d619c60a90f0e4eb26aa29e92665b83d836
Matthew Burger <mburger@stsci.edu>
2022-10-03 14:25:09 -0400

    Updated version number

--- MESSENGERuvvs/__init__.py
+++ MESSENGERuvvs/__init__.py
@@ -8,5 +8,5 @@ from .initialize_MESSENGERdata import initialize_MESSENGERdata
 name = 'MESSENGERuvvs'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '1.11.0'
-__date__ = '2022-09-12'
+__version__ = '1.11.1'
+__date__ = '2022-10-03'
